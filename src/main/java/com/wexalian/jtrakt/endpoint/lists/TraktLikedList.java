78235ff8ad26915504c5cfe1bc81820a8aaa91b4
Wexalian <wexalian@gmail.com>
2020-02-22 01:53:53 +0100

    Add missing endpoint

---
+++ src/main/java/com/wexalian/jtrakt/endpoint/lists/TraktLikedList.java
@@ -0,0 +1,25 @@
+package com.wexalian.jtrakt.endpoint.lists;
+
+import com.wexalian.jtrakt.endpoint.TraktList;
+
+public class TraktLikedList
+{
+    private int like_count;
+    private int comment_count;
+    private TraktList list;
+    
+    public int getLikeCount()
+    {
+        return like_count;
+    }
+    
+    public int getCommentCount()
+    {
+        return comment_count;
+    }
+    
+    public TraktList getList()
+    {
+        return list;
+    }
+}
